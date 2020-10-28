0de56bb8485041a52789071d24144d4404d93070
Yarek Tyshchenko <yarek@healthforge.io>
2020-10-28 13:47:36 +0000

    Fix accelearation

--- asteroids-server/src/server.ts
+++ asteroids-server/src/server.ts
@@ -28,57 +28,52 @@ interface Shell {
   ttl: number,
 }
 
+const MAX_SPEED = 1
+
 let shells: Shell[] = new Array<Shell>()
 const fire: (s: Ship) => Shell = ship => {
+  const shellVector = {
+    x: MAX_SPEED*2 * Math.sin(ship.bearing) + ship.velocity.x,
+    y: MAX_SPEED*2 * Math.cos(ship.bearing) + ship.velocity.y,
+  }
   return {
     owner: ship.id,
     bearing: ship.bearing,
     position: { x: ship.position.x, y: ship.position.y },
-    velocity: 1 + Math.hypot(ship.velocity.x, ship.velocity.y),
-    ttl: 2000,
+    velocity: Math.hypot(shellVector.x, shellVector.y),
+    ttl: 100,
   }
 }
 
 const ships: Map<String, Ship> = new Map<String, Ship>()
-//ships.set("A", {id: "A", bearing: 0, x: 42, y: 42, thrust: 1, turn: 0.01})
-
-const MAX_SPEED = 1
 
 const timer = setInterval(() => {
   for(const ship of ships.values()) {
     ship.bearing = ship.bearing + ship.rotation
-    // Calculate next x and y based on thrust and bearing
-    // ship.position.x = ship.position.x + ship.thrust * Math.sin(ship.bearing)
-    // ship.position.y = ship.position.y + ship.thrust * Math.cos(ship.bearing)
-    // velocity difference
-    const thrustVector = {x: 0.1 * Math.sin(ship.bearing), y: 0.1 * Math.cos(ship.bearing)}
-    //const currentSpeed = Math.hypot(ship.velocity.x, ship.velocity.y)
-     const targetSpeed = Math.hypot(thrustVector.x + ship.velocity.x, thrustVector.y + ship.velocity.y)
-    // const delta = targetSpeed - currentSpeed
-    const foo = (MAX_SPEED - targetSpeed) / 10
-    const correctedThrustVector = {
-      x: foo * ship.thrust * Math.sin(ship.bearing),
-      y: foo * ship.thrust * Math.cos(ship.bearing)
+
+    const thrustVector = {x: ship.thrust * Math.sin(ship.bearing), y: ship.thrust * Math.cos(ship.bearing)}
+    const finalVector = {x: thrustVector.x + ship.velocity.x, y: thrustVector.y + ship.velocity.y}
+    const finalBearing = Math.atan2(finalVector.x, finalVector.y)
+    let finalSpeed = Math.hypot(finalVector.x, finalVector.y)
+    if (finalSpeed > MAX_SPEED) {
+      finalSpeed = MAX_SPEED
     }
-    // Limit velocity
-    ship.velocity = {x: correctedThrustVector.x + ship.velocity.x, y: correctedThrustVector.y + ship.velocity.y}
-    // ship.velocity.x += ship.thrust * Math.sin(ship.bearing)
-    // ship.velocity.y += ship.thrust * Math.cos(ship.bearing)
+    ship.velocity.x = finalSpeed * Math.sin(finalBearing)
+    ship.velocity.y = finalSpeed * Math.cos(finalBearing)
+
     ship.position.x = ship.position.x + ship.velocity.x
     ship.position.y = ship.position.y + ship.velocity.y
 
   }
-  //console.log(ship)
   io.emit("ships", Array.from(ships.values()))
   for(const shell of shells.values()) {
-    // move shells along
     shell.position.x = shell.position.x + shell.velocity * Math.sin(shell.bearing)
     shell.position.y = shell.position.y + shell.velocity * Math.cos(shell.bearing)
     shell.ttl -= 1;
   }
   shells = shells.filter(s => s.ttl > 0)
   io.emit("shells", shells)
-}, 16.6)
+}, 1000/60)
 
 // whenever a user connects on port 3000 via
 // a websocket, log that a user has connected
@@ -92,7 +87,7 @@ io.on("connection", socket => {
   socket.on("command", command => {
     const ship: Ship = ships.get(id)!
     switch (command) {
-      case "thrust-start": ship.thrust = 1; break
+      case "thrust-start": ship.thrust = 0.1; break
       case "thrust-end": ship.thrust = 0; break
       case "turn-left": ship.rotation = 0.05; break
       case "turn-right": ship.rotation = -0.05; break
