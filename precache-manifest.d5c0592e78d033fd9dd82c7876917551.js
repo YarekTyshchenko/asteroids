fb30cb74c222b260e20e968600f5f39a08b4cbf9
Yarek Tyshchenko <yarek@healthforge.io>
2020-10-30 15:22:40 +0000

    Updates

---
+++ precache-manifest.d5c0592e78d033fd9dd82c7876917551.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b78083c958baa723ef3457658407c6",
+    "url": "/asteroids/index.html"
+  },
+  {
+    "revision": "8844c33bc9f1d871875c",
+    "url": "/asteroids/static/css/main.b16ea329.chunk.css"
+  },
+  {
+    "revision": "90e38cdca0c4596a7998",
+    "url": "/asteroids/static/js/2.bd3dff3e.chunk.js"
+  },
+  {
+    "revision": "4ebd4c934480c725a320d9506119b042",
+    "url": "/asteroids/static/js/2.bd3dff3e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8844c33bc9f1d871875c",
+    "url": "/asteroids/static/js/main.cd228f67.chunk.js"
+  },
+  {
+    "revision": "4e06987e367c75061ccf",
+    "url": "/asteroids/static/js/runtime-main.3e24850d.js"
+  }
+]);
