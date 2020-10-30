4de343ffb37703bc2212a4952be99296ca40991b
Yarek Tyshchenko <yarek@healthforge.io>
2020-10-30 18:29:45 +0000

    Use CNAME'd path

--- asteroids-client/src/providers/SocketContext.tsx
+++ asteroids-client/src/providers/SocketContext.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as io from "socket.io-client"
 
-const connection = io.connect(`https://asteroids-3ukqg.ondigitalocean.app/`)
+const connection = io.connect("https://asteroids.yarekt.co.uk")
 const SocketContext = React.createContext<SocketIOClient.Socket>(connection)
 const SocketConsumer = SocketContext.Consumer
 
